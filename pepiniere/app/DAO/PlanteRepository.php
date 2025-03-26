e6813dd87e91376d56e5d8ba609fb4977cbf04b7
Hounaida202 <etrichih@gmail.com>
2025-03-26 11:41:32 +0000

    la creation du fichier PlanteRepository qui contient la logique des requêtes vers la base de données

---
+++ pepiniere/app/DAO/PlanteRepository.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\DAO;
+use App\Models\Plante;
+use App\DAO\PlanteInterface;
+
+class PlanteRepository implements PlanteInterface
+{
+    public function getAllByCategorie($categorie_id)
+    {
+        return Plante::where('categories_id', $categorie_id)->get();
+    }
+
+    public function getBySlug($slug)
+    {
+        return Plante::where('slug', $slug)->first();
+    }
+
+    public function findById($id)
+    {
+        return Plante::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Plante::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $plante = Plante::findOrFail($id);
+        $plante->update($data);
+        return $plante;
+    }
+
+    public function delete($id)
+    {
+        $plante = Plante::findOrFail($id);
+        return $plante->delete();
+    }
+}
