d8f344cd6cae325b72999a0b01d480792fd0f846
Hounaida202 <etrichih@gmail.com>
2025-03-27 14:41:15 +0000

    reformulation de la methode de la generation des slugs avec spatie

--- pepiniere/app/Models/Plante.php
+++ pepiniere/app/Models/Plante.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Plante extends Model
 {
@@ -18,19 +20,27 @@ class Plante extends Model
         'categories_id'
     ];
 
-    protected static function booted()
-    {
-        static::creating(function ($plante) {
-            if (empty($plante->slug)) {
-                $plante->slug = Str::slug($plante->nom);
-            }
-        });
+    // protected static function booted()
+    // {
+    //     static::creating(function ($plante) {
+    //         if (empty($plante->slug)) {
+    //             $plante->slug = Str::slug($plante->nom);
+    //         }
+    //     });
 
-        static::updating(function ($plante) {
-            if (empty($plante->slug)) {
-                $plante->slug = Str::slug($plante->nom);
-            }
-        });
+    //     static::updating(function ($plante) {
+    //         if (empty($plante->slug)) {
+    //             $plante->slug = Str::slug($plante->nom);
+    //         }
+    //     });
+    // }
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('nom')
+            ->saveSlugsTo('slug');
     }
 
     public function categorie()
