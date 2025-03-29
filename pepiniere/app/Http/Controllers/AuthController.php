73cb4c1369338fedb75772f375da56e3a230a0db
Hounaida202 <etrichih@gmail.com>
2025-03-29 00:02:19 +0000

    la diagramme des sequences

--- pepiniere/app/Http/Controllers/AuthController.php
+++ pepiniere/app/Http/Controllers/AuthController.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      title="API de Gestion des Catégories",
+ *      version="1.0",
+ *      description="Documentation de l'API pour la gestion des catégories",
+ *      @OA\Contact(
+ *          email="support@example.com"
+ *      )
+ * )
+ */
+
+
+
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+
+/**
+ * @OA\Info(
+ *      title="API de Gestion des Catégories",
+ *      version="1.0",
+ *      description="Documentation de l'API"
+ * )
+ */
+
+/**
+ * @OA\Post(
+ *     path="/api/categories",
+ *     summary="Créer une nouvelle catégorie",
+ *     tags={"Catégories"},
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             required={"nom"},
+ *             @OA\Property(property="nom", type="string", example="Fruits")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=201,
+ *         description="Catégorie créée avec succès"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Données invalides"
+ *     )
+ * )
+ */
 
 class AuthController extends Controller
 
